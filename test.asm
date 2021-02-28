7169818c74afde7391127d91cfa537fd9cab1046
Sachin Chaulagai <50958807+chaulags@users.noreply.github.com>
2021-02-28 09:47:19 +0545

    testing hello world program

--- test.asm
+++ test.asm
@@ -1,1 +1,19 @@
-This is a test file
+;declaring data segment
+DATA SEGMENT
+     MESSAGE DB "* * * * * Welcome To Sachin Chaulagai's Calculator * * * * $"  ;storing the string value to MESSAGE variable
+ENDS
+
+;Declaring code segment
+CODE SEGMENT  
+    ASSUME DS:DATA CS:CODE ;Initializing data segment inside code segment
+START:
+      MOV AX,DATA ;transferring from DATA to AX register
+      MOV DS,AX ;transferring from AX to DS due to register restriction
+
+      LEA DX,MESSAGE ;writing the memory address of variable MESSAGE to DX register
+      MOV AH,9H
+      INT 21H
+      MOV AH,4CH
+      INT 21H      
+ENDS
+END START
